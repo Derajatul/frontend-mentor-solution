bb18869647b49f15db2ad24394f58ba31651a421
Derajatul <derajatul@gmail.com>
2023-05-01 07:14:21 +0700

    final four-card

--- Newbie/four-card-feature-section/src/components/Card.jsx
+++ Newbie/four-card-feature-section/src/components/Card.jsx
@@ -1,8 +1,8 @@
 const Card = (props) => {
   return (
-    <div className='card'>
+    <div className='card' style={{borderTop: `4px solid ${props.border}`}}>
       <h2 className='card-title'>{props.title}</h2>
-      <p className='card-sub-title'>{props.subTitle}</p>
+      <p className='card-sub-title thin'>{props.subTitle}</p>
       <img src={props.img} />
     </div>
   );
