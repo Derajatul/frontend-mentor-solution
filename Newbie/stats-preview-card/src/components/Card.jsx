a97f6efecaa9551271d79b6cbd92b328bd7d1414
Derajatul <derajatul@gmail.com>
2023-05-03 20:23:55 +0700

    beneran final

--- Newbie/stats-preview-card/src/components/Card.jsx
+++ Newbie/stats-preview-card/src/components/Card.jsx
@@ -11,12 +11,12 @@ const Card = () => {
       tipe: 'TEMPLATES',
     },
     {
-      jumlah: '12M',
+      jumlah: '12M+',
       tipe: 'QUERIES',
     },
   ];
   return (
-    <div className='card m-6 overflow-hidden rounded-xl bg-dark-desaturated-blue sm:flex sm:flex-row-reverse'>
+    <div className='card m-6 overflow-hidden rounded-xl bg-dark-desaturated-blue sm:flex sm:flex-row-reverse sm:items-center sm:justify-center'>
       <picture className='flex-1'>
         <source
           media='(min-width: 1024px)'
@@ -24,12 +24,12 @@ const Card = () => {
         />
         <img src='./image-header-mobile.jpg' alt='image header' />
       </picture>
-      <div className='m-8 flex-1'>
-        <h2 className='text-center text-2xl font-bold text-white'>
+      <div className='m-8 flex-1 sm:m-10'>
+        <h2 className='text-center text-2xl font-bold text-white sm:text-left sm:text-3xl'>
           Get <span className='text-soft-violet'>insights</span> that help your
           business grow.
         </h2>
-        <p className='my-5 text-center text-slightly-transparent-white'>
+        <p className='my-5 text-center text-slightly-transparent-white sm:text-left'>
           Discover the benefits of data analytics and make better decisions
           regarding revenue, customer experience, and overall efficiency.
         </p>
@@ -44,7 +44,7 @@ const Card = () => {
 };
 const Info = (props) => {
   return (
-    <div className='m-5 flex flex-col items-center'>
+    <div className='m-5 flex flex-col items-center sm:m-7'>
       <h3 className='text-2xl font-bold text-white'>{props.jumlah}</h3>
       <p className='text-slightly-transparent-white-stat'>{props.tipe}</p>
     </div>
